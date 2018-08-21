2506f4f3ed62ff40149a0f7c6d0c40d449146936
Sagiv Frankel <sagivf@gmail.com>
2018-08-21 11:37:25 +0300

    wip

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import {NavController} from '@ionic/angular';
+import {Component, ViewEncapsulation, NgZone} from '@angular/core';
 
-declare var OT:any;
-declare var Cordova: any;
+declare var OT: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomePage {
 
@@ -16,36 +15,36 @@ export class HomePage {
   apiKey: any;
   sessionId: string;
   token: string;
-  // 60e42bff5a310be0f2254cfbc14a241c1817d237
+  roomJoined = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private zone: NgZone) {
     this.apiKey = '46171312';
     this.sessionId = '1_MX40NjE3MTMxMn5-MTUzNDMxOTg1MTUwN35zdmtveDc5ZG1heitxUU5Ra3FYZzg0VVF-fg';
     this.token = 'T1==cGFydG5lcl9pZD00NjE3MTMxMiZzZGtfdmVyc2lvbj1kZWJ1Z2dlciZzaWc9YzNjNTMzMjA5NWFjODA5YTYxZDQ3NmRhNmZlZDBjYzhlZjMzMzcyMzpzZXNzaW9uX2lkPTFfTVg0ME5qRTNNVE14TW41LU1UVXpORE14T1RnMU1UVXdOMzV6ZG10dmVEYzVaRzFoZWl0eFVVNVJhM0ZZWnpnMFZWRi1mZyZjcmVhdGVfdGltZT0xNTM0MzE5ODUxJnJvbGU9bW9kZXJhdG9yJm5vbmNlPTE1MzQzMTk4NTEuNTIyNjExNjg0NzgyNjcmZXhwaXJlX3RpbWU9MTUzNjkxMTg1MQ==';
   }
 
-  startCall() {
-    alert('aaa')
+  leaveRoom() {
+    this.session.disconnect();
+    this.roomJoined = false;
+  }
+
+  joinRoom() {
     this.session = OT.initSession(this.apiKey, this.sessionId);
 
-    alert('bbbb')
     this.session.on({
       streamCreated: (event) => {
-        alert('ccc')
-        this.session.subscribe(event.stream, 'subscriber');
-        alert('dddd')
-        OT.updateViews();
+        this.session.subscribe(event.stream, 'subscriber', {
+          insertMode: 'append'
+        });
       },
-      streamDestroyed: (event) => {
-        console.log(`Stream ${event.stream.name} ended because ${event.reason}`);
-        OT.updateViews();
+      streamDestroyed: () => {
       }
     });
-    alert('111')
+
     this.session.connect(this.token, () => {
-      alert('222')
       this.publisher = OT.initPublisher('publisher');
       this.session.publish(this.publisher);
+      this.zone.run(() => this.roomJoined = true);
     });
   }
 }
